02560fd45e420e372d18afd08633563a9c8bfafc
Son Nguyen <sonnn.uct@gmail.com>
2021-11-25 10:20:05 +0700

    update

--- FindPhone.php
+++ FindPhone.php
@@ -3,7 +3,7 @@
 // $s = "098.777.7777. xxxx";
 // $s = "so dien thoai la 0978. 438 doi can nhe. 0987 987 654 day nhe";
 // $s = "0987 6567 xxxx xxx 87 xxxx 098 8987 765 xxxx";
-// $s = "0987 6567 xxxx xxx 87 xxxx 098 8987 765 x. xxx 0988888 888";
+$s = "0987 6567 xxxx xxx 87 xxxx 098 8987 765 x. xxx 0988888 888";
 // $s = "0987 6567 xxxx xxx 87 xxxx 8888767654543234 x. xxx 0988888 888";
 // $s = "0987 6567 xxxx xxx 87 xxxx 888876765. 4543234 xxxx. xxx .0988888 888";
 // $s = "8888888888888 097.999 99.     09 xxxxxx xxx";
@@ -12,12 +12,13 @@
 //  $s = "xxx 0979999998 0979999999. 09 xxxxxx xxx";
 // $s = "Địa chỉ của c là toà HH01A KĐT THANH HÀ. 0384468188";
 // $s = "Zalo dùng số điện thoại 0386500999";
-$s = "10/ 08- 2020. 09872 728 23 day ne";
+// $s = "10/ 08- 2020. 09872 728 23 day ne";
 $data = explode(" ", str_replace(".", " ", $s));
 
-check($data);
+$result = check($data);
+var_dump($result);
+function check($data, $from = 0, $result = []) {
 
-function check($data, $from = 0) {
     echo "Bắt đầu kiểm tra từ phần tử $from\n";
     $num = '';
     for ($i=$from;$i<count($data);$i++) {
@@ -34,17 +35,26 @@ function check($data, $from = 0) {
                 // kieu so
                 $num .= $data[$i];
                 echo "Day so hien tai dang co: $num \n";
+                
                 if (strlen($num)==10) {
                     // stop vi tim dc 10 so de check valid so dien thoại
                     echo "---------------------------------!!! MATCH: $num \n";
                     // check dau so valid 090, 091 092 .....
+                    
+                    // luu vao mang chua ket qua
+                    array_push($result, $num);
+
+                    // reset lai chuoi so de tim tiep
                     $num = ''; 
                     continue;
                 } else if (strlen($num)>10) {
                     // qua 10 so --> k phai la so dien thoại
                     $num = '';
                     echo "Đã lấy quá 10 số đây không phải là số điện thoại, reset để kiểm tra lại bắt đầu từ phần tử Mốc ".($milestone+1)."\n";
-                    check($data, $milestone+1);
+                    
+                    // tiep tuc tim kiem lai bat dau tu Mốc+1
+                    $result = check($data, $milestone+1, $result);
+                    
                     break;
                 }
             } else {
@@ -53,9 +63,10 @@ function check($data, $from = 0) {
                 // phan tu hien tai không phải là số --> duyet lai tu i+1
                 $num = ''; //reset ve de kiem tra lai
                 echo "Phần tử ".$i." không phải là dãy số, reset để kiểm tra lại bắt đầu từ phần tử ".($i+1)."\n";
-                check($data, $i+1);
+                $result = check($data, $i+1, $result);
                 break;
             }
         }
     }
+    return $result;
 }
