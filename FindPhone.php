81d037754fa18a49e37d2839e71084322932d01f
Son Nguyen <sonnn.uct@gmail.com>
2021-11-25 09:49:54 +0700

    update

--- FindPhone.php
+++ FindPhone.php
@@ -9,7 +9,10 @@
 // $s = "8888888888888 097.999 99.     09 xxxxxx xxx";
 // $s = "8888888888888 097.999.9999. 09 xxxxxx xxx";
 //  $s = "097.999.9998 097.999.9999. 09 xxxxxx xxx";
- $s = "xxx 0979999998 0979999999. 09 xxxxxx xxx";
+//  $s = "xxx 0979999998 0979999999. 09 xxxxxx xxx";
+// $s = "Địa chỉ của c là toà HH01A KĐT THANH HÀ. 0384468188";
+// $s = "Zalo dùng số điện thoại 0386500999";
+$s = "10/ 08- 2020. 09872 728 23 day ne";
 $data = explode(" ", str_replace(".", " ", $s));
 
 check($data);
@@ -22,6 +25,11 @@ function check($data, $from = 0) {
         // echo $i;
         if (!empty($data[$i])) {
             if (is_numeric($data[$i])) {
+                if ($num==''){
+                    // luu lai mốc phần tử đang kiểm tra
+                    $milestone = $i;
+                    echo "(Mốc checking: $milestone)\n";
+                }
                 echo "Tim duoc day so: ----> ".$data[$i] ."\n";
                 // kieu so
                 $num .= $data[$i];
@@ -35,11 +43,13 @@ function check($data, $from = 0) {
                 } else if (strlen($num)>10) {
                     // qua 10 so --> k phai la so dien thoại
                     $num = '';
-                    echo "Đâ lấy quá 10 số đây không phải là số điện thoại, reset để kiểm tra lại bắt đầu từ phần tử ".($i+1)."\n";
-                    check($data, $i+1);
+                    echo "Đã lấy quá 10 số đây không phải là số điện thoại, reset để kiểm tra lại bắt đầu từ phần tử Mốc ".($milestone+1)."\n";
+                    check($data, $milestone+1);
                     break;
                 }
             } else {
+                
+                
                 // phan tu hien tai không phải là số --> duyet lai tu i+1
                 $num = ''; //reset ve de kiem tra lai
                 echo "Phần tử ".$i." không phải là dãy số, reset để kiểm tra lại bắt đầu từ phần tử ".($i+1)."\n";
